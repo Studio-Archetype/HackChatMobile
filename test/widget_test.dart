bfc4d6e230eb23d8aa309c25f657502c2da32069
Liz <lizainslie16@gmail.com>
2021-11-28 00:13:37 -0600

    fix the test import

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:HackChat/main.dart';
+import 'package:Chat.Bytecode.Ninja/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
